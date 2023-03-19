478f21551d1052c2e81b94f4bc302e02fa255515
Rachana1104 <y.rachana1104@gmail.com>
2023-03-19 17:16:21 +0530

    react-router-dom added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import './App.css';
-
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Courses from './components/Courses';
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+    <Router>
+      <Routes>
+        <Route path="/courses" element={<><Courses/></>}></Route>
+      </Routes>
+    </Router>
+    </>
   );
 }
 
